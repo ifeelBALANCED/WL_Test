f0ae6daf16c8245b4decf980207a899ef7fa7cd2
vlad <vladislavherasymenko@gmail.com>
2021-05-20 02:12:47 +0300

    fix: bugs with modal(create new little modal)

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
-import './style.css'
-
+import './style.css';
 
 const fetchUsers = async () => {
     try {
-        const url = "https://jsonplaceholder.typicode.com/users";
+        const url = 'https://jsonplaceholder.typicode.com/users';
         const res = await fetch(url);
         const users = await res.json();
-        initialize(users)
-        const trs = document.querySelectorAll('tbody > tr')
-        trs.forEach(tr => tr.addEventListener('click', (evt) => {
-            const modalBody = document.querySelector('.modal-body'),
-                modalTitle = document.querySelector('.modal-title'),
-                trValue = evt.currentTarget.innerText
-            const [num, name, username, email, site] = trValue.split('\t')
-            modalTitle.textContent = 'Detailed info'
-            modalBody.textContent = `
-            № - ${num},
-            Name - ${name},
-            Username - ${username},
-            Email - ${email},
-            Website - ${site}
-            `
-            openModal()
-        }))
+        initialize(users);
+        const trs = document.querySelectorAll('tbody > tr');
+        trs.forEach(tr =>
+            tr.addEventListener('click', evt => {
+                renderModal(evt);
+            })
+        );
     } catch ({message}) {
-        console.log(`An error has occurred: ${message}`)
+        console.log(`An error has occurred: ${message}`);
     }
 };
 
+const renderModal = evt => {
+    const detailedModalTitle = document.querySelector('.detailed-modal-title'),
+        detailedModalBody = document.querySelector('.detailed-modal-body'),
+        trValue = evt.currentTarget.innerText;
+    const [num, name, username, email, site] = trValue.split('\t');
+    detailedModalTitle.textContent = 'Detailed info';
+    detailedModalBody.textContent = `
+      № - ${num},
+      Name - ${name},
+      Username - ${username},
+      Email - ${email},
+      Website - ${site}
+  `;
+    showDetailedModal();
+};
+
 const initialize = users => {
-    let wrapper = document.querySelector("tbody");
-    users.map(
-        ({name, username, email, website, id}) => {
-            wrapper.innerHTML += `
+    let wrapper = document.querySelector('tbody');
+    users.map(({name, username, email, website, id}) => {
+        wrapper.innerHTML += `
          <tr>
            <td>${id}</td>
            <td>${name}</td>
@@ -42,14 +46,11 @@ const initialize = users => {
               <a class="effect">${website}</a>
            </td>
          </tr>
-        `
-        }
-    );
-}
-
+        `;
+    });
+};
 
 fetchUsers();
-
 
 const openModal = () => modal.classList.add('visible');
 
@@ -59,9 +60,58 @@ const openModalButtons = document.querySelectorAll('.open-modal'),
     closeModalButtons = document.querySelectorAll('.close-modal');
 
 openModalButtons.forEach(openBtn => {
-    openBtn.addEventListener('click', openModal)
+    openBtn.addEventListener('click', openModal);
 });
 
+const onSubmit = event => {
+    event.preventDefault();
+    let name = document.querySelector('#name').value,
+        username = document.querySelector('#username').value,
+        email = document.querySelector('#email').value,
+        website = document.querySelector('#website').value,
+        form = document.querySelector('.modal-body'),
+        tbody = document.querySelector('tbody'),
+        allRows = document.querySelectorAll('tbody > tr');
+    form.reset();
+    if (!name || !username || !email || !website) return;
+
+    let newTr = document.createElement('tr');
+    newTr.addEventListener('click', evt => {
+        renderModal(evt);
+    });
+    newTr.innerHTML += `
+           <td>${allRows.length + 1}</td>
+           <td>${name}</td>
+           <td>${username}</td>
+           <td>${email}</td>
+           <td>
+              <a class="effect">${website}</a>
+           </td>
+    `;
+    tbody.insertBefore(newTr, tbody.lastElementChild.nextSibling);
+    closeModal();
+};
+
 closeModalButtons.forEach(closeBtn => {
-    closeBtn.addEventListener('click', closeModal)
+    closeBtn.addEventListener('click', closeModal);
+});
+
+const submitBtn = document.querySelector('.submit-btn');
+submitBtn.addEventListener('click', onSubmit);
+
+const overlay = document.getElementById('overlay'),
+    detailedModal = document.getElementById('detailed-modal'),
+    closeBtn = document.getElementById('close-btn')
+
+const showDetailedModal = () => {
+    overlay.classList.add('is-visible');
+    detailedModal.classList.add('is-visible');
+}
+closeBtn.addEventListener('click', () => {
+    overlay.classList.remove('is-visible');
+    detailedModal.classList.remove('is-visible');
+});
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('is-visible');
+    detailedModal.classList.remove('is-visible');
 });
